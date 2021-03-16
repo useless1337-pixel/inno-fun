12b4dd9dde4c767238fd7172a764856f47148104
Innocent <69703402+Innocent1337@users.noreply.github.com>
2021-03-16 02:53:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "stupid idiot." << endl;
+	cout << " _                           .____             " << endl;
+	cout << " | , __   , __     __.       /     ,   . , __  " << endl;
+	cout << " | |'  `. |'  `. .'   \      |__.  |   | |'  `." << endl;
+	cout << " | |    | |    | |    |      |     |   | |    |" << endl;
+	cout << " / /    | /    |  `._.'      /     `._/| /    |" << endl;
+	cout << "                                               " << endl;
+
+	SetConsoleTitleA("Inno Fun");
+
+	int rndmNumber = 1337;
 }
